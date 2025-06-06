70563ccdba886be063d9b95d4d4d168cc18b707f
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2025-06-06 17:55:35 -0300

    Update sprite assets and adjust motobug animation settings
    - Replaced the motobug sprite image and updated the run animation settings to increase the number of frames from 5 to 8 for improved animation fluidity.
    - Ensured consistency in sprite management by updating related graphics files.

--- src/main.js
+++ src/main.js
@@ -98,10 +98,10 @@ for (let i = 0; i <= 10; i++) {
 // })
 
 k.loadSprite("motobug", "graphics/motobug.png", {
-  sliceX: 5,
+  sliceX: 8,
   sliceY: 1,
   anims: {
-    run: { from: 0, to: 4, loop: true, speed: 8 },
+    run: { from: 0, to: 7, loop: true, speed: 8 },
   },
 })
 k.loadFont("mania", "fonts/mania.ttf")
